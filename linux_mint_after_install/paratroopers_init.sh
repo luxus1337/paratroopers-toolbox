6574fe74168eed075c5d12643d619fca08c6c261
Luigi van der Pal <eentweedrie@gmail.com>
2020-01-22 14:45:45 +0100

    Update paratroopers_init.sh

--- linux_mint_after_install/paratroopers_init.sh
+++ linux_mint_after_install/paratroopers_init.sh
@@ -20,9 +20,8 @@ sudo snap install dbeaver-ce
 sudo apt install -y filezilla
 
 # composer
-sudo apt install php
+sudo apt -y install php
 curl -sS https://getcomposer.org/installer -o composer-setup.php
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # node
